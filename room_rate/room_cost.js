d3503b38a2f9feb341a7c6d0617089dadb6bd194
pilimou <soulms55@gmail.com>
2026-03-06 16:40:22 +0800

    修改貓談社每晚折扣

--- room_rate/room_cost.js
+++ room_rate/room_cost.js
@@ -2,19 +2,28 @@
     
     // --- A. 從後端抓取資料並產生 Checkbox ---
     function loadExtraServices() {
-        // 這裡換成您的 API 網址
         $.ajax({
-            url: 'https://mycathotelrepo.onrender.com/queryRoomCost', 
+            url: 'https://mycathotelrepo.onrender.com/queryRoomPrice', 
             type: 'POST',
             dataType: 'json',
             success: function(data) {
                 const $list = $('#extra-services-list');
                 $list.empty(); // 清除載入中文字
-
-                $.each(data, function(i, item) {
-                    // item 格式假設為 { id: 1, name: "剪指甲", price: 100 }
+                $.each(data.roomCosts, function(i, item) {
                     const html = `
                         <label class="service-item">
+                            <input type="checkbox" class="extra-check" 
+                                    data-name="${item.costName}" 
+                                    data-price="${item.costPrice}" value="${item.id}">
+                            <span style="margin-left:10px;">${item.costName}</span>
+                            <span class="service-price">$${item.costPrice}</span>
+                        </label>
+                        `;
+                    $list.append(html);
+                });
+                $.each(data.roomCostEveryNights, function(i, item) {
+                    const html = `
+                        <label class="service-item2">
                             <input type="checkbox" class="extra-check" 
                                     data-name="${item.costName}" 
                                     data-price="${item.costPrice}" value="${item.id}">
